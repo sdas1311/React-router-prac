aee2b43f1fa16dee4971445e4787199f27916b62
Soumya Das <dasronisoumya13@gmail.com>
2024-06-27 23:25:53 +0530

    'first commit'

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Home from './components/home/Home'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  
+  return (
+    <>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+
+    </>
+  )
+}
+
+export default App
