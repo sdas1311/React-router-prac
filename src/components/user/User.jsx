dbee4a7e0670a4e7cbcfa8c3560fae8b236a4a5e
Soumya Das <dasronisoumya13@gmail.com>
2024-06-29 13:56:58 +0530

    2nd day of react learning, added github component

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function User() {
+  const {userid} = useParams()
   return (
-    <div>User</div> 
+    <div className='bg-gray-600 text-white text-3xl p-4 flex justify-center'>User : {userid}</div> 
   )
 }
 
