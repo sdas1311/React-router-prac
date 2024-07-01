ab17ed8fd02925003e4db40617711f58f68470f6
Soumya Das <dasronisoumya13@gmail.com>
2024-07-01 22:36:43 +0530

    "react router done"

--- src/components/github/Github.jsx
+++ src/components/github/Github.jsx
@@ -1,18 +1,26 @@
 import React, { useEffect, useState } from 'react'
-
+import { useLoaderData } from 'react-router-dom'
 function Github() {
-    const [data,setData] = useState([])
-    useEffect(()=>{
-        fetch('https://api.github.com/users/sdas1311')
-        .then(response => response.json())
-        .then(data => {
-            console.log (data);
-            setData(data)
-        })
-    },[])
+  const data = useLoaderData()  
+  
+  // const [data,setData] = useState([])
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/sdas1311')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log (data);
+    //         setData(data)
+    //     })
+    // },[])
   return (
-    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl' >Github followers: {data.followers} </div>
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl ' >Github followers: {data.followers} 
+    <div className='flex justify-center mt-4'><img src={data.avatar_url} alt="Git picture" width={300} height={300} /></div>
+    </div>
   )
 }
 
 export default Github
+export const githubInfoLoader =async () => {
+  const response =await fetch('https://api.github.com/users/sdas1311')
+  return await response.json();
+}
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Home from './components/home/Home.jsx'
 import About from './components/about/About.jsx'
 import Contact from './components/contact/Contact.jsx'
 import User from './components/user/User.jsx'
-import Github from './components/github/Github.jsx'
+import Github, {githubInfoLoader} from './components/github/Github.jsx'
 
 // const router = createBrowserRouter ([
 //   {
@@ -37,7 +37,10 @@ const router = createBrowserRouter(
       <Route path='about' element={<About/>}/>
       <Route path='contact' element={<Contact/>}/>
       <Route path='user/:userid' element={<User/>}/>
-      <Route path='github' element={<Github/>}/>
+      <Route
+       loader={(githubInfoLoader)}
+       path='github'
+       element={<Github/>}/>
     </Route>
   )
 )
